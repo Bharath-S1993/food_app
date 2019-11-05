2a981099daad25820e31d610392177ebfddb325a
Bharath <bharathsj143@gmail.com>
2019-11-05 22:54:51 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping/shopping-list/shopping-edit/shopping-edit.component';
+import { ReceipesComponent } from './receipes/receipes.component';
+import { ReceipesListComponent } from './receipes/receipes-list/receipes-list.component';
+import { ReceipesItemComponent } from './receipes/receipes-list/receipes-item/receipes-item.component';
+import { ReceipesDetailsComponent } from './receipes/receipes-details/receipes-details.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    ReceipesComponent,
+    ReceipesListComponent,
+    ReceipesItemComponent,
+    ReceipesDetailsComponent,
   ],
   imports: [
     BrowserModule,
